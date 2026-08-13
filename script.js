d0a3ecd2e29fe111a6d244ded1540303d32e976d
beatrizlimalara-ops <beatriz.lima.lara@escola.pr.gov.br>
2026-08-13 09:21:10 -0300

    Implement card flip function for flashcard animation
    Adds a function to toggle the 'flipped' class on flashcard elements for CSS animation.

---
+++ script.js
@@ -0,0 +1,7 @@
+/**
+ * Alterna a classe '.flipped' no elemento do flashcard para disparar a animação 3D em CSS.
+ * @param {HTMLElement} cardElement - O elemento .flashcard que recebeu o clique.
+ */
+function flipCard(cardElement) {
+    cardElement.classList.toggle('flipped');
+}
